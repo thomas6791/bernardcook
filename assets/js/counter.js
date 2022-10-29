a25e579b52e095e05286e39dea35c37feb160e0c
thomas hunckler <hunckler.thomas@hotmail.fr>
2022-10-29 16:24:16 +0200

    change

--- assets/js/counter.js
+++ assets/js/counter.js
@@ -1,1 +1,20 @@
 console.log("Bonjour");
+let btns = document.querySelectorAll(".order-btn");
+btns.forEach((btn) => {
+    btn.addEventListener("click", (event) => {
+        if (event.currentTarget.classList.contains("plus")){
+            let counterData = event.currentTarget.parentElement.querySelector(".counter").innerHTML;
+            let counter = parseFloat(counterData);
+            counter = counter += 1;
+            event.currentTarget.parentElement.querySelector(".counter").innerHTML = counter;
+        }
+        else {
+            let counterData = event.currentTarget.parentElement.querySelector(".counter").innerHTML;
+            let counter = parseFloat(counterData);
+            counter = counter -= 1;
+            event.currentTarget.parentElement.querySelector(".counter").innerHTML = counter;
+
+        }
+      })
+  })
+console.log(btns);
